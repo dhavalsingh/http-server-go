48bdcbe03326965dd56a781f0c51611434bfddcd
Dhaval <anarock@MacBook-Pro.local>
2023-11-13 01:40:45 +0530

    pass 4th  stage

--- app/server.go
+++ app/server.go
@@ -8,25 +8,7 @@ import (
 	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
-
+func handleConnection (conn){
 	buffer := make([]byte, 1024)
 	// buffer, err := io.ReadAll(conn)
 	_, err = conn.Read(buffer)
@@ -65,4 +47,25 @@ func main() {
 		fmt.Println("Error sending msg: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage
+	
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+	go handleConnection(conn)
 }
