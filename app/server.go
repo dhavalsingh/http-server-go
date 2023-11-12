58e247ae101637ebdd9cd6e343e777440b061dcb
Dhaval <anarock@MacBook-Pro.local>
2023-11-13 00:09:26 +0530

    pass 3st stage

--- app/server.go
+++ app/server.go
@@ -26,7 +26,29 @@ func main() {
 	}
 	defer conn.Close()
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 1024)
+	// buffer, err := io.ReadAll(conn)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)	
+	}
+	request := string(buffer)
+	lines := strings.Split(request, "\r\n")
+	start_line := lines[0]
+	start_line_parts = strings.Fields(start_line)
+	path := start_line_parts[1]
+
+	var response string
+
+	if path == "/"{
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	}
+	else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	_, err = conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error sending msg: ", err.Error())
 		os.Exit(1)
